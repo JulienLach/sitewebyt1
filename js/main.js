ac8770c0b004eba16f74bfbfc7a83588dcf254af
JulienLach <leadedchamps@gmail.com>
2023-11-06 12:28:49 +0100

    OK

--- js/main.js
+++ js/main.js
@@ -40,3 +40,18 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(groupHeader);
     });
 });
+
+
+
+// Mobile menu
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const hamburgerButton = document.querySelector('.hamburger-button');  // ici définir les constante que je vais animer en cliquant dessus , ici le boutton et le menu en lui même
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    hamburgerButton.addEventListener('click', () =>
+        mobileMenu.classList.toggle('active')
+    );
+});
